09830990ad9ec449546bed2aa17d7b808cb7b7c1
NemisRuparel <nemisruparel07@gmail.com>
2025-11-30 09:47:56 +0530

    implement dock

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 import Navbar from "#components/Navbar.jsx";
 import Welcome from "#components/welcome";
+import Dock from "#components/Dock";
 
-export {Navbar, Welcome};
+export {Navbar, Welcome, Dock};
