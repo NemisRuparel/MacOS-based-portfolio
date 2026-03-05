2feec7dde766e06254f2d86713d5e83b9869b337
NemisRuparel <nemisruparel07@gmail.com>
2026-03-05 13:52:32 +0530

    fix: correct import path for Welcome component and format export statement

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,8 @@
 import Navbar from "#components/Navbar.jsx";
-import Welcome from "#components/welcome";
+import Welcome from "#components/Welcome.jsx";
 import Dock from "#components/Dock";
 import window from "#store/window";
 import WindowControls from "./WindowControls";
 import WindowClose from "./WindowClose";
-export {Navbar, Welcome, Dock, window, WindowControls, WindowClose};
+
+export { Navbar, Welcome, Dock, window, WindowControls, WindowClose };
